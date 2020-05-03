1ffa9e46243d200e71e4cb1834832876ea3afe18
vitaliy.boytsov <vitaliy.boytsov@onetick.com>
2020-05-03 17:25:50 +0300

    ads

--- a.py
+++ a.py
@@ -1,26 +1,16 @@
+import os
 import random
-import numpy as np
-from matplotlib import pyplot as plt
+
 import matplotlib.pyplot as plt
-
-from skimage import filters
-from skimage.data import camera
-from skimage.util import compare_images
-from scipy import misc
-import glob
+import numpy as np
 from PIL import Image
-from skimage import filters
 from scipy import signal
-from scipy import misc
-
 
 EPS = 1e-5
 
 
-
 def normalize(A):
     return A / max(abs(min(0, A.min())), A.max())
-
 
 
 def conv(input, step):
@@ -32,6 +22,7 @@ def conv(input, step):
             ans[i][j] = np.amin(input[i * step: i * step + step, j * step: j * step + step])
     return ans
 
+
 def convert_image(input):
     return np.vectorize(lambda v: v / 255)(input)
 
@@ -39,14 +30,8 @@ def convert_image(input):
 def convert_phi(input):
     return np.vectorize(lambda v: 1 - v / 127.5)(input)
 
-def open_c_png():
-    I = np.asarray(Image.open("c.png").convert('L'))
-    # I = conv(I, 4)
-    I = convert_image(I)
-    return I
-
-def open_nodule_png():
-    I = np.asarray(Image.open("nodule.png").convert('L'))
+def open_png(file_name):
+    I = np.asarray(Image.open(file_name + ".png").convert('L'))
     # I = conv(I, 4)
     I = convert_image(I)
     return I
@@ -57,48 +42,34 @@ def open_phi_png():
     phi_0 = convert_phi(phi_0)
     return phi_0
 
-I = open_c_png()
-# I = open_nodule_png()
-phi_0 = open_phi_png()
-n = I.shape[0]
-m = I.shape[1]
-T = 150
+def draw_plt(I, n = 64, m = 64):
+    ## initialize of phi_0
 
-# phi_0 = np.eye(n)
-# phi_0 = np.array([[random.random() for _ in range(n)] for _ in range(n)])
+    # phi_0 = open_phi_png()
+    # phi_0 = np.eye(n)
+    phi_0 = np.array([[random.random() for _ in range(n)] for _ in range(n)])
 
 
-def read_images():
-    I = np.asarray(Image.open("individualImage.png").convert('L'))[2:-2, 2:-2]
-    I_list = []
-    for i in range(8):
-        for j in range(8):
-            I_list.append(I[i * 66: i * 66 + 64, j * 66: j * 66 + 64])
-            I_list[-1] = convert_image(I_list[-1])
-    return I_list
-
-list_I = read_images()
-
-for ind, I in enumerate(list_I):
-    if ind > 10:
-        break
     phi_list = [phi_0.copy()]
 
+    ## initialize of matrixes
     U_g, W_g, U_z, W_z, U_r, W_r, U_o, W_o, V = [np.eye(n) for _ in range(9)]
-    # U_z /= 8
-    # U_g /= 10
-    # U_r /= 8
-    # U_o /= 10
     b_z, b_r, b_o, b_V = [np.zeros(n) for _ in range(4)]
 
+
+    ## initialize of plot shape
     rows = 3
     cols = 4
     T = rows * cols
 
-    fig, axes = plt.subplots(ncols=cols, nrows=rows, sharex=True, sharey=True, figsize=(cols * 5, rows * 5))
-    axes[0][0].imshow(I, cmap=plt.cm.gray)
-    axes[0][0].set_title('I')
+    def add_im(I, t, title):
+        ob = axes[t // cols][t % cols].imshow(I, cmap=plt.cm.gray)
+        axes[t // cols][t % cols].set_title(title)
+        cbar = fig.colorbar(ob, ax=axes[t // cols][t % cols], extend='both')
+        cbar.minorticks_on()
 
+    fig, axes = plt.subplots(ncols=cols, nrows=rows, sharex=True, sharey=True, figsize=(cols * 5, rows * 5))
+    add_im(I, 0, "I")
 
     for t in range(1, T):
         def sigmoid(u):
@@ -110,6 +81,7 @@ for ind, I in enumerate(list_I):
 
         SOBEL_X = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
         SOBEL_Y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
         def img_derivative(input, sobel_kernel):
             return signal.convolve2d(input, sobel_kernel, boundary='symm', mode="same")
 
@@ -125,10 +97,9 @@ for ind, I in enumerate(list_I):
             # return filters.sobel(phi) # it works odd, but fairly good
 
             # return -(phi_dx_n + phi_dy_n) # np.abs should be used, without it we get mars photos
-            return -(np.abs(phi_dx_n) + np.abs(phi_dy_n)) # np.abs should be used, without it we get mars photos
+            return -(np.abs(phi_dx_n) + np.abs(phi_dy_n))  # np.abs should be used, without it we get mars photos
             #
             # return normalize(-(np.abs(phi_dx_n) + np.abs(phi_dy_n))) # it should be normalized, it is visible with I=c_png
-
 
         def g(I, phi):
             c1 = 0  # average values of inside of the contour
@@ -143,22 +114,46 @@ for ind, I in enumerate(list_I):
             c2 /= (n * m - counter) if (n * m - counter) else 1
             return curve(phi) - U_g @ (10 * I - c1) ** 2 + W_g @ (10 * I - c2) ** 2
 
-
         x_t = g(I, phi_list[t - 1])
-
 
         z_t = np.vectorize(sigmoid)(U_z @ x_t + W_z @ phi_list[t - 1] + b_z)  # update gate zt
         r_t = np.vectorize(sigmoid)(U_r @ x_t + W_r @ phi_list[t - 1] + b_r)  # reset gate rt
         o_t = np.vectorize(np.tanh)(U_o @ x_t + W_o @ (phi_list[t - 1] * r_t) + b_o)  # intermediate hidden unit ht
 
-        phi_list.append((1 - z_t) * o_t + z_t * phi_list[t - 1]) # should be trained
-        # phi_list.append(z_t * phi_list[t - 1]) # temporary, focus only what inside phi
+        phi_list.append((1 - z_t) * o_t + z_t * phi_list[t - 1])  # should be trained
         phi_now = np.vectorize(lambda v: 0 if v < 0 else 1)(phi_list[t])
-        axes[t // cols][t % cols].imshow(phi_now, cmap=plt.cm.gray)
-        # axes[t // cols][t % cols].imshow(phi_list[t], cmap=plt.cm.gray)
-        axes[t // cols][t % cols].set_title('phi_' + str(t))
+        add_im(phi_list[t], t, "phi_" + str(t + 1))
 
-    plt.savefig("rls_test_" + str(ind + 1) + ".png")
+
+## reading one image
+# I = open_png("nodule")
+# draw_plt(I)
+# plt.savefig(file_name + ".png")
+# plt.show()
+
+
+def read_images(file):
+    I = np.asarray(Image.open(file + ".png").convert('L'))[2:-2, 2:-2]
+    I_list = []
+    for i in range(8):
+        for j in range(8):
+            I_list.append(I[i * 66: i * 66 + 64, j * 66: j * 66 + 64])
+            I_list[-1] = convert_image(I_list[-1])
+    return I_list
+
+## images with many samples
+file_name_1 = "individualImage"
+file_name_2 = "individualImage-2"
+file_name = file_name_1
+
+list_I = read_images(file_name)
+
+## cycle for image in list_I
+for ind, I in enumerate(list_I):
+    draw_plt(I)
+    if not os.path.exists(file_name):
+        os.makedirs(file_name)
+    plt.savefig(file_name + "/rls_test_" + str(ind) + ".png")
     plt.show()
 
 print("done")
